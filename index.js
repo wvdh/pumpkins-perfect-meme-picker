ccdb717b6de98ed4a4f9ee9638f535e076510d8d
Wanda <wanda@vandenhoogen.eu>
2024-11-29 22:15:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const emotionRadios = document.getElementById('emotion-radios')
 emotionRadios.addEventListener('change', highlightCheckedOption)
 
 function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+        for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
