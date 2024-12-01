3540e2eec94da071ee8a77f6530e29dfca5e2b09
Wanda <wanda@vandenhoogen.eu>
2024-12-01 23:12:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { catsData } from './data.js'
+
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
 const gifsOnlyOption = document.getElementById('gifs-only-option')
 const memeModalInner = document.getElementById('meme-modal-inner')
-const memeModal = document.getElementById('meme-modal') 
+const memeModal = document.getElementById('meme-modal')
+const memeModalCloseBtn = document.getElementById('meme-modal-close-btn') 
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
+memeModalCloseBtn.addEventListener('click', closeModal)
 getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
@@ -14,6 +17,35 @@ function highlightCheckedOption(e){
         radio.classList.remove('highlight')
     }
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
+
+function closeModal(){
+    memeModal.style.display = 'none'
+}
+
+function renderCat(){
+    const catObject = getSingleCatObject()
+        memeModalInner.innerHTML = `
+        <img 
+        class="cat-img" 
+        src="./images/${catObject.image}"
+        alt="${catObject.alt}"
+        >
+        `
+        memeModal.style.display = 'flex'
+}
+
+function getSingleCatObject(){
+    
+    const catsArray = getMatchingCatsArray()
+    console.log(catsArray)
+
+    if (catsArray.length === 1) {
+        return catsArray[0]
+    } else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
 }
 
 function getMatchingCatsArray(){     
@@ -32,23 +64,6 @@ function getMatchingCatsArray(){
         })
         return matchingCatsArray 
     }   
-}
-
-function getSingleCatObject(){
-    
-    const catsArray = getMatchingCatsArray()
-    console.log(catsArray)
-
-    if (catsArray.length === 1) {
-        return catsArray[0]
-    } else {
-        const randomNumber = Math.floor(Math.random() * catsArray.length)
-        return catsArray[randomNumber]
-    }
-}
-
-function renderCat(){
-    getSingleCatObject()
 }
 
 function getEmotionsArray(cats){
