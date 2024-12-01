7f1dc62e07e78b416daf0d6597cf1d7b918595ee
Wanda <wanda@vandenhoogen.eu>
2024-12-01 20:06:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import { catsData } from './data.js'
 const emotionRadios = document.getElementById('emotion-radios')
 const getImageBtn = document.getElementById('get-image-btn')
-const gifsOnlyOption = document.getElementById('gifs-only-option') 
+const gifsOnlyOption = document.getElementById('gifs-only-option')
+const memeModalInner = document.getElementById('meme-modal-inner')
+const memeModal = document.getElementById('meme-modal') 
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
-
-getImageBtn.addEventListener('click', getMatchingCatsArray)
+getImageBtn.addEventListener('click', renderCat)
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -21,15 +22,33 @@ function getMatchingCatsArray(){
         const isGif = gifsOnlyOption.checked
         
         const matchingCatsArray = catsData.filter(function(cat){
-                      
-            if (isGif) {
+            
+            if(isGif){
                 return cat.emotionTags.includes(selectedEmotion) && cat.isGif
-            } else {
-                return cat.emotionTags.includes(selectedEmotion)
             }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }            
         })
-        console.log(matchingCatsArray)
-    }  
+        return matchingCatsArray 
+    }   
+}
+
+function getSingleCatObject(){
+    
+    const catsArray = getMatchingCatsArray()
+    console.log(catsArray)
+
+    if (catsArray.length === 1) {
+        return catsArray[0]
+    } else {
+        const randomNumber = Math.floor(Math.random() * catsArray.length)
+        return catsArray[randomNumber]
+    }
+}
+
+function renderCat(){
+    getSingleCatObject()
 }
 
 function getEmotionsArray(cats){
@@ -63,5 +82,4 @@ function renderEmotionsRadios(cats){
     emotionRadios.innerHTML = radioItems
 }
 
-renderEmotionsRadios(catsData)
 renderEmotionsRadios(catsData)
